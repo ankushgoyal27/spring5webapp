6b929f946bde0a576503bd3024614f055787db2f
Ankush <ankushgoyal11@gmail.com>
2019-05-28 15:36:52 -0400

    Create BookRepository.java

---
+++ src/main/java/com/springpractice/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.springpractice.spring5webapp.repositories;
+
+
+import com.springpractice.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
